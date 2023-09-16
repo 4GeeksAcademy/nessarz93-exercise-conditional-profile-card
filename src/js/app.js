5804dd9f2f25d3061d6ba778fdea0f34f3024caa
Vanessa <126210620+nessarz93@users.noreply.github.com>
2023-09-16 01:45:35 +0000

    Links personalizados agregados

--- src/js/app.js
+++ src/js/app.js
@@ -37,10 +37,10 @@ function render(variables = {}) {
           <h2>${variables.role}</h2>
           <h3>${variables.city}, ${variables.country}</h3>
           <ul class= "${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${variables.name} ${variables.lastname}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.name} ${variables.lastname}"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.name} ${variables.lastname}"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.name} ${variables.lastname}y"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
